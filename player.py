532b5bd4e61df96602dc54e8d3b97a664e0a1c22
rafancoding <24chowdhuryraf@student.bournemouth-school.org>
2025-04-03 19:09:21 +0100

    commit

--- player.py
+++ player.py
@@ -23,16 +23,17 @@ class Player(pygame.sprite.Sprite):
         self.collision("horizontal")
         self.hitbox_rect.y += self.direction.y * self.speed * dt
         self.collision("vertical")
+        self.rect.center = self.hitbox_rect.center
 
     def collision(self,direction):
         for sprite in self.collision_sprites:
-            if sprite.rect.colliderect(self.hitbox_rect):
+            if sprite.rect.colliderect(self.rect):
                 if direction == "horizontal":
-                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
-                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+                    if self.direction.x > 0: self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.rect.left = sprite.rect.right
                 else:
-                    if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
-                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top 
+                    if self.direction.y < 0: self.rect.top = sprite.rect.bottom
+                    if self.direction.y > 0: self.rect.bottom = sprite.rect.top 
 
     def update(self,dt):
         self.input()
