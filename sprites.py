4ffd01853f36e714f9f167cc8675705527f46d59
rafancoding <24chowdhuryraf@student.bournemouth-school.org>
2025-04-05 12:00:21 +0100

    collisions

--- sprites.py
+++ sprites.py
@@ -4,10 +4,10 @@ class NonCollisionSprites(pygame.sprite.Sprite):
     def __init__(self,pos,surf,groups):
         super().__init__(groups)
         self.image = surf
-        self.rect = self.image.get_frect(center = pos)
+        self.rect = self.image.get_frect(topleft = pos)
 
 class CollisionSprites(pygame.sprite.Sprite):
     def __init__(self,pos,surf,groups):
         super().__init__(groups)
         self.image = surf
-        self.rect = self.image.get_frect(center = pos)
+        self.rect = self.image.get_frect(topleft = pos)
