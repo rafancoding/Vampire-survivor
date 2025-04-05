a0c1b2fbd01a4c79e60d68ee20a0f1c0d9800db1
rafancoding <24chowdhuryraf@student.bournemouth-school.org>
2025-04-05 21:57:15 +0100

    commmit

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups,collision_sprites):
         super().__init__(groups)
+        self.animation()
         self.image = pygame.image.load("images/player/down/0.png").convert_alpha()
         self.rect = self.image.get_frect(center = pos)
         self.hitbox_rect = self.rect.inflate(-60,-90)
