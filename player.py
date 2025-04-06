4a4a2f232969e06ed699cdc351a7e502cc109543
rafancoding <24chowdhuryraf@student.bournemouth-school.org>
2025-04-06 10:30:11 +0100

    commit

--- player.py
+++ player.py
@@ -62,7 +62,7 @@ class Player(pygame.sprite.Sprite):
 
         #animate
         self.frame_index += self.frame_index + 5 * dt if self.direction else 0
-        self.image = self.frames[self.state][float(self.frame_index) % len(self.frames[self.state])]
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
 
 #updating
     def update(self,dt):
