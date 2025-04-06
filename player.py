85868adf878e80efea1bc7b41f3db7d8a2d6a83f
rafancoding <24chowdhuryraf@student.bournemouth-school.org>
2025-04-06 10:33:04 +0100

    comiit

--- player.py
+++ player.py
@@ -62,7 +62,7 @@ class Player(pygame.sprite.Sprite):
             self.state = "down" if self.direction.y > 0 else "up"    
 
         #animate
-        self.frame_index += self.frame_index + 5 * dt if self.direction else 0
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0
         self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
 
 #updating
