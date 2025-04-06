4af45687f955076d0d94e111c96e0d1df3e5086d
rafancoding <24chowdhuryraf@student.bournemouth-school.org>
2025-04-06 11:08:45 +0100

    mouse

--- sprites.py
+++ sprites.py
@@ -20,13 +20,20 @@ class Gun(pygame.sprite.Sprite):
     def __init__(self,player,groups):
         self.player = player
         self.distance = 140
-        self.player_direction = pygame.Vector2(1,0)
+        self.player_direction = pygame.Vector2(0,1)
 
         super().__init__(groups)
         self.gun_surf = pygame.image.load("images/gun/gun.png").convert_alpha()
         self.image = self.gun_surf
         self.rect = self.image.get_frect(center = self.player.rect.center + self.player_direction * self.distance)
+    
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.player_direction = (mouse_pos - player_pos).normalize()
+        print(self.player_direction)
 
     def update(self,_):
+        self.get_direction  
         self.rect.center = self.player.rect.center + self.player_direction * self.distance 
 
