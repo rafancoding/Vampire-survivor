0fe15ea609c2178d84cf685111315bbc69907b8c
rafancoding <24chowdhuryraf@student.bournemouth-school.org>
2025-04-08 19:49:08 +0100

    commit

--- main.py
+++ main.py
@@ -78,10 +78,11 @@ class Game:
     def bullet_collision(self):
         if self.bullet_sprites:
             for bullet in self.bullet_sprites:
-                collided_sprites = pygame.sprite.spritecollide(bullet,self.enemy_sprites,True,pygame.sprite.collide_mask)
-                if collided_sprites:
-                    for sprite in collided_sprites:
+                collision_sprites = pygame.sprite.spritecollide(bullet,self.enemy_sprites,False,pygame.sprite.collide_mask)
+                if collision_sprites:
+                    for sprite in collision_sprites:
                         sprite.destroy()
+                    bullet.kill()    
                     
     #map setup
     def map(self):
